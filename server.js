754462797eebaeee67f60d2f6f4cf5bb3dfd6927
Alice <ora202503@gmail.com>
2025-12-15 15:35:28 +0800

    u

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// 静态网页
+app.use(express.static('public'));
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Hello World app running on port ${PORT}`);
+});
+
